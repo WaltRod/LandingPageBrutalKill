abb20b381742ad61e219b0833a0cb0fe94be74bd
Walter E. Rodrigues <111158948+WaltRod@users.noreply.github.com>
2022-12-02 18:32:26 -0300

    Upando arquivos

---
+++ script.js
@@ -0,0 +1,10 @@
+function Entrar(){
+    var a = document.getElementById('enter')
+    a.style.background = 'rgb(206, 33, 119)'
+    a.style.color = 'white'
+}
+function Sair(){
+    var a = document.getElementById('enter')
+    a.style.background = 'white'
+    a.style.color = 'black'
+}
